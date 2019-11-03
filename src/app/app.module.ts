0b1254a72f8344c2acc809f0cee34c0267274482
GreatToubib <basilburleon@hotmail.com>
2019-11-03 18:56:46 +0100

    ajout de components et des vues dans le profil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ProfileTabsComponent } from './profile-tabs/profile-tabs.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AccueilComponent } from './accueil/accueil.component';
+
+
+const appRoutes: Routes = [
+  { path: 'profile',component: ProfileTabsComponent , data: {title:'page title1'} }, // :id is a route parameter and data is to parse static data
+  { path: 'accueil',component: AccueilComponent , data: {title:'page title2'} },
+  { path: '', component: AccueilComponent} //  The router will select this route if the requested URL doesn't match any paths for routes defined earlier in the 
+   //                                      configuration. This is useful for displaying a "404 - Not Found" page or redirecting to another route. It MUST BE THE LAST ROUTE
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileTabsComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
